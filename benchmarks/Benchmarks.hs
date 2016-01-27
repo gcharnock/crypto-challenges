4be9d74842cd5dd38e4b6400b975f60066278257
Gareth Charnock <gareth.charnock@gmail.com>
2016-01-27 23:38:28 +0000

    Challenge 1 + benchmark. Still needs test.

--- benchmarks/Benchmarks.hs
+++ benchmarks/Benchmarks.hs
@@ -4,21 +4,19 @@ module Main where
 import Criterion.Types (Config(..))
 import Criterion.Main (defaultMainWith, bench, whnf, bgroup, defaultConfig)
 
-fib :: Int -> Int
-fib m = go m
-  where
-    go 0 = 0
-    go 1 = 1
-    go n = go (n-1) + go (n-2)
+import Lib
+
+fibs :: Num a => [a]
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+fib :: (Num a) => Int -> a
+fib n = fibs !! n
 
 config :: Config
 config = defaultConfig { reportFile = Just "benchmark.html"}
 
 main :: IO ()
 main = defaultMainWith config [
-  bgroup "fib" [ bench "1"  $ whnf fib 1
-               , bench "5"  $ whnf fib 5
-               , bench "9"  $ whnf fib 9
-               , bench "11" $ whnf fib 11
+  bgroup "challenge 1" [ bench "challenge_1_base64bytestring" $ whnf challenge_1_base64bytestring inputVector
                ]
   ]
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,18 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE OverloadedStrings #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib where
+
+import Data.ByteString(ByteString)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Base64 as Base64
+import qualified Data.ByteString.Base16 as Base16
+
+
+inputVector :: ByteString
+inputVector = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+targetVector :: ByteString
+targetVector = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+
+challenge_1_base64bytestring :: ByteString -> ByteString
+challenge_1_base64bytestring = Base64.encode . fst . Base16.decode
