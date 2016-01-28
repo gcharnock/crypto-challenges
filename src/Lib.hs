db6b933f865a4ea884d2897ff2cb59b9f6bd91ac
Gareth Charnock <gareth.charnock@ocado.com>
2016-01-28 18:43:52 +0000

    Setup unit test framework.

--- src/Lib.hs
+++ src/Lib.hs
@@ -17,23 +17,23 @@ import qualified Data.Vector.Storable as V
 import Foreign.ForeignPtr
 import Data.Bits
 
-inputVector :: ByteString
-inputVector = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+{-
 
-targetVector :: ByteString
-targetVector = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+The maths for base16 to base64 convertions is as follows:
+
+Each input group of 3 inputs symbols (x0, x1, x2) produces 2
+output symbols
+
+y0 = x0        + (x1 << 4) % 64
+y1 = (x1 >> 2) + (x2 << 2) % 64
+
+Remembering that x0 is the least significant symbol
+
+-}
+
 
 challenge_1_base64bytestring :: ByteString -> ByteString
 challenge_1_base64bytestring = Base64.encode . fst . Base16.decode
-
-xorString1 :: ByteString
-xorString1 = "1c0111001f010100061a024b53535009181c"
-
-xorString2 :: ByteString
-xorString2 = "686974207468652062756c6c277320657965"
-
-xorTarget :: ByteString
-xorTarget = "746865206b696420646f6e277420706c6179"
 
 byteStringToVector :: ByteString -> V.Vector CChar
 byteStringToVector bs = unsafePerformIO $ do
@@ -49,6 +49,5 @@ vectorToByteString v = unsafePerformIO $ do
 
 
 challenge_2_xor :: ByteString -> ByteString -> ByteString
-challenge_2_xor s1 s2 = vectorToByteString $ V.zipWith xor (decode s1) (decode s2)
+challenge_2_xor s1 s2 = Base16.encode $ vectorToByteString $ V.zipWith xor (decode s1) (decode s2)
  where decode = byteStringToVector . fst . Base16.decode
-       
